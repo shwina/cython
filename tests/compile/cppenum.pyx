f746a3b035bb25724354bab75c00670bf61b4e3f
Ashwin Srinath <shwina@users.noreply.github.com>
2020-05-20 09:28:57 -0400

    Add cppenum compile test

--- tests/compile/cppenum.pyx
+++ tests/compile/cppenum.pyx
@@ -1,33 +1,17 @@
 # mode: compile
-# distutils: language=c++
+# tag: cpp
 
-cdef extern from * namespace "Blah":
-    """
-    #include <iostream>
+cpdef enumclass Spam:
+    a
+    b
+    c
+    d
+    e
+    f = 42
 
-    namespace Blah {
-    enum class Spam : bool {
-        a = true,
-        b = false
-    };
+cdef void eggs():
+    cdef Spam s1
+    s1 = Spam.a
+    s2 = Spam.b
 
-    void print_spam(Spam x) {
-        if (x == Spam::a)
-            std::cout << "a" << std::endl;
-        else
-            std::cout << "b" << std::endl;
-    }
-    }
-
-    """
-    cpdef enumclass Spam:
-        a
-        b
-
-    void print_spam(Spam)
-
-def py_print_spam(Spam x):
-    print_spam(x)
-
-py_print_spam(Spam.a)
-py_print_spam(Spam.b)
+eggs()
